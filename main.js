550f3f520c815902748dd24c04826892e309fd2f
Maxime Gouillardon <maxime.gouillardon@gmail.com>
2022-10-08 21:08:26 +0200

    feat validator js

--- main.js
+++ main.js
@@ -1,2 +1,31 @@
-import '/main.css';
+import "/main.css";
 
+let age = document.getElementById("age");
+
+let height = document.getElementById("height");
+
+let weight = document.getElementById("weight");
+
+let caloriesResult = document.getElementById('calories');
+
+let calculateBtn = document.querySelector('button');
+
+const calculateTDEE = (gender, age, height, weight, activityLevel) => {
+
+    if(gender == "male") {
+        return (10 * weight + 6.25 * height - 5 * age + 5) * activityLevel;
+    }
+    
+    return (10 * weight + 6.25 * height - 5 * age - 161) * activityLevel;
+
+}
+
+calculateBtn.addEventListener('click', () => {
+    let genderValue = document.querySelector("input[name='gender']:checked").value;
+
+    let activityLevelValue = document.querySelector("input[name='activityLevel']:checked").value;
+
+    let TDEE = Math.trunc(calculateTDEE(genderValue, age.value, height.value, weight.value, activityLevelValue)) ;
+
+    caloriesResult.innerHTML = TDEE;
+})
